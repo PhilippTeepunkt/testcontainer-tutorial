e8ffad2e4532e6ca61ecf8b7458638dadf8be602
Philipp Tornow <philipp.tornow@uni-weimar.de>
2019-06-30 00:01:14 +0200

    added basic contact classes

---
+++ src/main/java/JDBC.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class JDBC {
+
+    private String url;
+    private String user;
+    private String pw;
+
+    private Connection connection;
+
+        public JDBC(String url, String user, String pw) throws SQLException {
+        this.url = url;
+        this.user = user;
+        this.pw = pw;
+
+        connection = DriverManager.getConnection(this.url, this.user, this.pw);
+    }
+
+    //used to send query to database and get result set
+    public ResultSet submitQuery(String query) throws  SQLException
+    {
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(query);
+        return result;
+    }
+
+    //used to update the database date (insert, delete)
+    public void update(String query) throws SQLException
+    {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(query);
+    }
+}
