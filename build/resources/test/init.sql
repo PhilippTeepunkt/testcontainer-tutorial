d62ed361f37330a68bd3be33ec97c52544d2eba4
Philipp Tornow <philipp.tornow@uni-weimar.de>
2019-06-30 21:10:18 +0200

    accept buildfiles to get pushed for testing purpose

---
+++ build/resources/test/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE contacts(ID int(100) NOT NULL AUTO_INCREMENT PRIMARY KEY ,NAME VARCHAR(45),VORNAME VARCHAR(45),AGE int(3));
+INSERT INTO contacts (ID,NAME,VORNAME,AGE)
+    VALUES (1,'Mustermann','Max',24),(2,'Müller','Manfred',35);
+
